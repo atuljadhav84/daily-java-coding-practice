b19e317792d83ef636f6951d5109a571a1f7f57b
atuljadhav84 <jadhavatul3526@gmail.com>
2026-05-05 22:33:09 +0530

    Compound Assignment Operator Example
    Compound Assignment Operator Example

---
+++ CompoundAssignmentOpeEXaple.java
@@ -0,0 +1,108 @@
+class CompoundAssignmentOpeEXaple{
+    public static void main ( String [] args ){
+        // 1
+        int a = 10;
+        a+= 10;
+        System.out.println(a);
+
+        //2
+        int b = 10; 
+        b *= 2; 
+        System.out.println(b);  
+
+        //3
+        byte c = 10; 
+        c = c += 5; 
+        System.out.println(c);
+
+        //4
+        byte d = 10; 
+        d += 5;
+        System.out.println(d);
+
+        //5
+        byte e = 10; 
+        e += 5; 
+        System.out.println(e);
+
+        //6
+        int f = 20; 
+        f -= 10; 
+        System.out.println(f);
+
+        //7
+        int g = 5; 
+        g *= 3; 
+        System.out.println(g);
+
+        //8
+        int h = 10; 
+        h /= 2; 
+        System.out.println(h);
+
+        //9
+        int i = 10; 
+        i %= 3; 
+        System.out.println(i);
+
+        //10
+        byte j = 100; 
+        j += 30; 
+        System.out.println(j);
+
+        //11
+        int k = 5; 
+        k += k + 5; 
+        System.out.println(k);
+
+        //12
+        int l = 10; 
+        l -= l - 3; 
+        System.out.println(l);
+
+        //13
+        int m = 5; 
+        m *= m + 2; 
+        System.out.println(m);
+
+        //14
+        int n = 20; 
+        n /= n / 2; 
+        System.out.println(n);
+
+        //15
+        int o = 10; 
+        o += 5 * 2; 
+        System.out.println(o);
+
+        //16
+        int p = 10; 
+        p *= 5 + 2; 
+        System.out.println(p);
+
+        //17
+        int q = 8; 
+        q *= 3 + 2; 
+        System.out.println(q);
+
+        //18
+        int r = 6; 
+        r += 4 * 3; 
+        System.out.println(r);
+
+        //19
+        int s = 20; 
+        s -= 5 + 5; 
+        System.out.println(s);
+
+        //20
+        int t = 15; 
+        t /= 3 + 2; 
+        System.out.println(t);
+
+        //21
+        int u = 25; 
+        u %= 4 + 3; 
+        System.out.println(u);
+    }
+}
